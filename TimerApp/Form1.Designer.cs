612105446b6abf44aeb8d30c61765e06294c8a80
Augustin Onta <onta.augustin@isa.utm.md>
2023-02-23 00:51:09 +0200

    Add mouse activity tracking

--- TimerApp/Form1.Designer.cs
+++ TimerApp/Form1.Designer.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.IO;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace TimerApp
 {
     public partial class Form1 : Form
     {
-        private Timer timer = new Timer();
+        private Point lastCursorPosition;
+        private Timer inactivityTimer = new Timer();
+        private Timer inactivityTimer2 = new Timer();
+        private Timer userTimer = new Timer();
         private Timer logTimer = new Timer();
         private int seconds = 0;
         private int minutes = 0;
@@ -16,20 +20,66 @@ namespace TimerApp
         public Form1()
         {
             InitializeComponent();
-            timer.Interval = 1000;
-            timer.Tick += Timer_Tick;
+
+            // Inițializăm ultimele coordonate ale mouse-ului cu poziția curentă
+            lastCursorPosition = Cursor.Position;
 
 
+            userTimer.Interval = 1000;
+            userTimer.Tick += Timer_Tick;
+
+
+            // Creem fisier-ul log
             string logFileName = $"cronometru-log-{DateTime.Now:yyyyMMdd-HHmmss}.txt";
             logFile = new StreamWriter(logFileName, append: true);
 
-            logTimer.Interval = 10000; //la cite ms sa faca loguri in fisier (10 sec)
+            logTimer.Interval = 10000; // 10 sec do log
             logTimer.Tick += LogTimer_Tick;
             logTimer.Start();
 
 
+            inactivityTimer.Interval = 1000; // Interval de 1 secundă
+            inactivityTimer.Tick += inactivityTimer_Tick;
+            inactivityTimer.Start();
+
+            inactivityTimer2.Interval = 5000; // Interval de 10 secundă pentru avertizare
+            inactivityTimer2.Tick += inactivityTimer2_Tick;
+            inactivityTimer2.Start();
+
+
             FormClosing += Form1_FormClosing;
         }
+     
+        private void inactivityTimer_Tick(object sender, EventArgs e)
+        {
+            {
+                // Verificăm dacă poziția mouse-ului s-a schimbat față de ultima poziție
+                if (lastCursorPosition != Cursor.Position)
+                {
+                    // Actualizăm ultimele coordonate ale mouse-ului
+                    lastCursorPosition = Cursor.Position;
+                    //timerul se reporneste
+                    inactivityTimer2.Stop();
+                    inactivityTimer2.Start();
+                }
+
+            }
+        }
+        private void inactivityTimer2_Tick(object sender, EventArgs e)
+        {
+            inactivityTimer.Stop();
+            inactivityTimer2.Stop();
+            userTimer.Stop();
+            // Afișăm un mesaj de avertizare cu MessageBox
+            DialogResult result = MessageBox.Show("Are you here?", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            if (result == DialogResult.OK)
+            {
+                inactivityTimer.Start();
+                inactivityTimer2.Start();
+                userTimer.Start();
+            }
+        }
+
 
         private void LogTimer_Tick(object sender, EventArgs e)
         {
@@ -60,7 +110,7 @@ namespace TimerApp
             string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - Cronometru sa pornit!";
             logFile.WriteLine(logMessage);
 
-            timer.Start();
+            userTimer.Start();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -71,6 +121,5 @@ namespace TimerApp
 
         private Button button1;
         private Label labelTime;
-        private System.ComponentModel.IContainer components;
     }
 }
