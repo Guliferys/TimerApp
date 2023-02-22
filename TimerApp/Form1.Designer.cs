27f69f31be148af4130bd8b90caa45365d418ffd
user <user@ALEXANDERSON>
2023-02-22 14:06:07 +0200

    Add project files.

---
+++ TimerApp/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TimerApp
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer = new Timer();
+        private int seconds = 0;
+        private int minutes = 0;
+        private int hours = 0;
+        private StreamWriter logFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+
+            string logFileName = $"cronometru-log-{DateTime.Now:yyyyMMdd-HHmmss}.txt";
+            logFile = new StreamWriter(logFileName, append: true);
+
+            Timer logTimer = new Timer();
+            logTimer.Interval = 10000; //la cite ms sa faca loguri in fisier (10 sec)
+            logTimer.Tick += LogTimer_Tick;
+
+
+            FormClosing += Form1_FormClosing;
+        }
+
+        private void LogTimer_Tick(object sender, EventArgs e)
+        {
+            string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - Timpul curent este {hours:00}:{minutes:00}:{seconds:00}";
+            logFile.WriteLine(logMessage);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            if (seconds == 60)
+            {
+                seconds = 0;
+                minutes++;
+                if (minutes == 60)
+                {
+                    minutes = 0;
+                    hours++;
+                }
+            }
+
+            labelTime.Text = $"{hours:00}:{minutes:00}:{seconds:00}";
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            logTimer.Start();
+
+            string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - Aplicatia a pornit!";
+            logFile.WriteLine(logMessage);
+
+            timer.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            logFile.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - Aplicatia sa oprit!");
+            logFile.Close();
+        }
+
+        private Button button1;
+        private Label labelTime;
+        private System.ComponentModel.IContainer components;
+    }
+}
---
+++ TimerApp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerApp
+{
+    public partial class Form1 : Form
+    {
+
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(59, 48);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(174, 46);
+            this.labelTime.TabIndex = 1;
+            this.labelTime.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
